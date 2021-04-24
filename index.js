85e67dc73f2d22ad1d632907b514883ff8b6c336
Adikko <adriankulik93@gmail.com>
2021-04-24 15:16:08 +0200

    animation attempt 5

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function activateXR() {
           reticle.position.set(hitPose.transform.position.x, hitPose.transform.position.y, hitPose.transform.position.z)
           reticle.updateMatrixWorld(true);
           // animate over time
-          clone.scale.y = (Math.sin(elapsedTime*0.3));
+          clone.scale.y = (Math.sin(elapsedTime*0.3) + (Math.PI * 0.37) * 100);
       }
 
       // Render the scene with THREE.WebGLRenderer.
