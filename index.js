a67a2627056214003274c047690ebcd298dac27e
Adikko <adriankulik93@gmail.com>
2021-05-12 16:05:31 +0200

    add antialias, changed size of the reticle

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ async function activateXR() {
   const renderer = new THREE.WebGLRenderer({
     alpha: true,
     preserveDrawingBuffer: true,
+    antialias: true,
     canvas: gameLoop_canvas,
     context: gl
   });
@@ -103,7 +104,7 @@ async function activateXR() {
 
   // Reticle helps the user with placing the 3D object in the scene
   reticle = new THREE.Mesh(
-    new THREE.RingGeometry( 0.4, 0.5, 4 ).rotateX(-Math.PI / 2),
+    new THREE.RingGeometry( 0.2, 0.25, 4 ).rotateX(-Math.PI / 2),
     new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } )
   );
   reticle.visible = false;
