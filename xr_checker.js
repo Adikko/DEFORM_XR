bd8db4fcaf045428c9863fe9cc2c114414a58759
Adikko <adriankulik93@gmail.com>
2021-05-12 17:13:24 +0200

    button now checks if the navigator supports xr

---
+++ xr_checker.js
@@ -0,0 +1,24 @@
+// First let's check if xr functionality is availible
+let xr_checker = null;
+if ("xr" in navigator) {
+    xr_checker = true;
+} else {
+    xr_checker = false;
+}
+
+// Let's edit the button to display the correct call to action
+const init_button = document.getElementsByClassName("deform_button_container")[0];
+const instructions_list = document.getElementById("instructions");
+if (xr_checker === true) {
+    init_button.innerHTML = '<button class="deform_xr_button" onclick="activateXR()">Start!</button>'
+} else {
+    let explanation = document.createElement("li");
+    instructions_list.appendChild(explanation);
+    if (window.navigator.language === "pl-PL") {
+        init_button.innerHTML = '<button class="deform_xr_button" disabled">Niewspierane</button>';
+        explanation.innerHTML = '<h2 id="instruction_2">Wypróbuj przeglądarkę Google Chrome na smartfonie z Androidem</h2>'
+    } else {
+        init_button.innerHTML = '<button class="deform_xr_button" disabled">AR not supported</button>';
+        explanation.innerHTML = '<h2 id="instruction_2">Try Google Chrome on an Android Smartphone</h2>';
+    }
+}
